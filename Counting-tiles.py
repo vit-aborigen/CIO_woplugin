f0f8ab1216f119a97758e384656bbc27059ad4f9
vit-aborigen <vit.aborigen@gmail.com>
2017-05-20 16:03:21 +0300

    Middle: counting tiles

--- Counting-tiles.py
+++ Counting-tiles.py
@@ -1,9 +1,40 @@
 import math
 
 def checkio(radius):
+    """
+    Checking only right top corner (all four corners are symmetric
+    :param radius: 
+    :return list of bool, whether corner in circle 
+    """
     length = int(math.ceil(radius))
+
+    def isCornersInCircle(tiles: tuple, R: int):
+        """
+        :param tiles: left_bottom, right_top corners of tile. If both corners are inside the circle
+                      then tile is fully covered by circle, 1 corner covered - partially
+        :param R: circle radius
+        :return: tuple of bool - is each corners of tile covered? 
+        """
+        result = []
+        for tile in tiles:
+            result.append(math.sqrt(tile[0]**2 + tile[1]**2) <= radius)
+        return result
+
+
+    full_squares, partial_squares = 0, 0
+
     for i in range(length):
         for j in range(length):
+            left_bottom = (i,j)
+            right_top = (i+1, j+1)
+            tile_result = isCornersInCircle((left_bottom, right_top), radius)
+            if sum(tile_result) == 1: partial_squares +=1
+            elif sum(tile_result) == 2: full_squares += 1
+    return [full_squares, partial_squares]
+
+print(checkio(2))
+
+
 
 
 
