35cfef086c4989c32dd439ae3b99a9f5ec9b9e4d
vit-aborigen <vit.aborigen@gmail.com>
2018-10-10 01:21:34 +0300

    Time converter done

---
+++ Time Converter (24h to 12h).py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+def time_converter(time):
+    dt = datetime.strptime(time, "%H:%M")
+    return dt.strftime("%I:%M %p")
+
+if __name__ == '__main__':
+    print("Example:")
+    print(time_converter('12:30'))
+
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert time_converter('12:30') == '12:30 p.m.'
+    assert time_converter('09:00') == '9:00 a.m.'
+    assert time_converter('23:15') == '11:15 p.m.'
+    print("Coding complete? Click 'Check' to earn cool rewards!")
