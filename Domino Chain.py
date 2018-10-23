a5001423ceeb98f46fb52260056aa6050005d177
vit-aborigen <vit.aborigen@gmail.com>
2018-10-24 00:17:21 +0300

    Domino chains done

--- Domino Chain.py
+++ Domino Chain.py
@@ -1,48 +1,51 @@
-def build_graph(dominos):
-    graph = {(domino[0] + domino[2]): set() for domino in dominos.replace(',', '').split(' ')}  # Build nodes only
-    connected_nodes = []
-    for domino_value in range(7):
-        connected_nodes.append({node for node in graph if str(domino_value) in node})  # Make a link between edges
+import copy
+DOMINO_MAX_VALUE = 6
+DEBUG = False
 
-    for nodes in filter(lambda x: len(x) > 1, connected_nodes):
-        for node in nodes:
-            graph[node] = graph[node].union((nodes - {node}))  # Add edges between nodes
-    return graph
+def log(string):
+    if DEBUG:
+        if isinstance(string[0], list):
+            for text in string:
+                print(text)
+            print()
+        else:
+            print(string)
 
-def remove_edges(graph, tile):
-    pass
+def build_matrix(tiles):
+    matrix = [[0 for _ in range(DOMINO_MAX_VALUE + 1)] for _ in range(DOMINO_MAX_VALUE + 1)]
+    for domino in tiles.split(', '):
+        i, j = map(int, domino.split('-'))
+        matrix[i][j] = matrix[j][i] = 1
+    log(matrix)
+    return matrix
 
+def check_solution(row, source_matrix):
+    if not sum(sum(row) for row in source_matrix):
+        return 1
+    solutions_count = 0
 
-def find_all_paths(graph, start, path=[]):
-    path = path + [start]
-    if len(path) == len(graph):
-        yield path
-    if start not in graph:
-        return
-
-    for node in graph[start]:
-        if node not in path:
-            yield from find_all_paths(graph, node, path)
-
+    for index in range(DOMINO_MAX_VALUE + 1):
+        log('row: {}, index: {}, value: {}'.format(row, index, source_matrix[row][index]))
+        if source_matrix[row][index]:
+            source_matrix[row][index] = source_matrix[index][row] = 0
+            solutions_count += check_solution(index, source_matrix)
+            source_matrix[row][index] = source_matrix[index][row] = 1
+    return solutions_count
 
 def domino_chain(tiles: str) -> int:
-    graph = build_graph(tiles)
-    print(graph)
-    pathes = []
-    for node in graph:
-        for path in find_all_paths(graph, node):
-            if path[::-1] not in pathes:
-                pathes.append(path)
-    print(pathes)
-    # return sum([1 for node in graph for path in find_all_paths(graph, node)])
+    matrix = build_matrix(tiles)
+    counter = 0
+    for row in range(DOMINO_MAX_VALUE + 1):
+        counter += check_solution(row, matrix)
+    return counter // 2
 
 
 if __name__ == '__main__':
     # These "asserts" using only for self-checking and not necessary for auto-testing
-    # print(domino_chain("1-5, 2-5, 3-5, 4-5, 3-4"))
+    print(domino_chain("1-2, 0-1, 2-6, 0-0, 5-5, 6-6, 5-6, 4-4, 0-6, 1-5, 1-1, 1-6, 0-5, 3-6, 0-4, 2-5, 2-4"))
     # assert domino_chain("0-2, 0-5, 1-5, 1-3, 5-5") == 1
     # assert domino_chain("1-5, 2-5, 3-5, 4-5, 3-4") == 2
-    assert domino_chain("0-5, 1-5, 2-5, 3-5, 4-5, 3-4") == 0
+    # assert domino_chain("0-5, 1-5, 2-5, 3-5, 4-5, 3-4") == 0
     # assert domino_chain("0-1, 0-2, 1-3, 1-2, 3-4, 2-4") == 6
     # assert domino_chain("0-1, 0-2, 1-3, 1-2, 3-4, 2-4, 3-0, 0-4") == 0
     # assert domino_chain("1-2, 2-2, 2-3, 3-3, 3-1") == 5
