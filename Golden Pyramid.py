e39b0a633afab1a038d99970ae7888d04d08bd13
vit-aborigen <vit.aborigen@gmail.com>
2018-11-06 00:13:32 +0300

    Golden pyramid done

---
+++ Golden Pyramid.py
@@ -0,0 +1,39 @@
+def recursive(i, j, pyramid):
+    if i == len(pyramid) - 1:
+        return pyramid[i][j]
+    left = recursive(i + 1, j, pyramid)
+    right = recursive(i + 1, j + 1, pyramid)
+    if left >= right:
+        return left + pyramid[i][j]
+    return right + pyramid[i][j]
+
+def count_gold(pyramid):
+    return recursive(0,0,pyramid)
+
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert count_gold((
+        (1,),
+        (2, 3),
+        (3, 3, 1),
+        (3, 1, 5, 4),
+        (3, 1, 3, 1, 3),
+        (2, 2, 2, 2, 2, 2),
+        (5, 6, 4, 5, 6, 4, 3)
+    )) == 23, "First example"
+    assert count_gold((
+        (1,),
+        (2, 1),
+        (1, 2, 1),
+        (1, 2, 1, 1),
+        (1, 2, 1, 1, 1),
+        (1, 2, 1, 1, 1, 1),
+        (1, 2, 1, 1, 1, 1, 9)
+    )) == 15, "Second example"
+    assert count_gold((
+        (9,),
+        (2, 2),
+        (3, 3, 3),
+        (4, 4, 4, 4)
+    )) == 18, "Third example"
