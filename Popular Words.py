cea3678c73ef8f9382e115c07b9f3ca35fe5490d
vit-aborigen <vit.aborigen@gmail.com>
2018-11-06 23:11:25 +0300

    Popular words

---
+++ Popular Words.py
@@ -0,0 +1,27 @@
+def popular_words(text: str, words: list) -> dict:
+    formatted_text = text.lower().replace('\n', ' ').split(' ')
+    return {word: formatted_text.count(word) for word in words}
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(popular_words('''
+When I was One
+I had just begun
+When I was Two
+I was nearly new
+''', ['i', 'was', 'three', 'near']))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert popular_words('''
+When I was One
+I had just begun
+When I was Two
+I was nearly new
+''', ['i', 'was', 'three', 'near']) == {
+        'i': 4,
+        'was': 3,
+        'three': 0,
+        'near': 0
+    }
+    print("Coding complete? Click 'Check' to earn cool rewards!")
