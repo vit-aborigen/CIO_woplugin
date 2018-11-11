b7871b8f47f04d575b7f992eddebcd44d540f12b
vit-aborigen <vit.aborigen@gmail.com>
2018-11-11 22:45:53 +0300

    Morse Decoder done

---
+++ Morse Decoder.py
@@ -0,0 +1,30 @@
+MORSE = {'.-':    'a', '-...':  'b', '-.-.':  'c',
+         '-..':   'd', '.':     'e', '..-.':  'f',
+         '--.':   'g', '....':  'h', '..':    'i',
+         '.---':  'j', '-.-':   'k', '.-..':  'l',
+         '--':    'm', '-.':    'n', '---':   'o',
+         '.--.':  'p', '--.-':  'q', '.-.':   'r',
+         '...':   's', '-':     't', '..-':   'u',
+         '...-':  'v', '.--':   'w', '-..-':  'x',
+         '-.--':  'y', '--..':  'z', '-----': '0',
+         '.----': '1', '..---': '2', '...--': '3',
+         '....-': '4', '.....': '5', '-....': '6',
+         '--...': '7', '---..': '8', '----.': '9',
+        }
+
+def morse_decoder(code):
+    result = ''
+    for word in code.split('   '):
+        for char in word.split():
+            result += MORSE[char]
+        result += ' '
+    return (result[0].upper() + result[1:-1]) if result[0].isalpha() else result[:-1]
+
+if __name__ == '__main__':
+    print(morse_decoder("... --- -- .   - . -..- -"))
+
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert morse_decoder("... --- -- .   - . -..- -") == "Some text"
+    assert morse_decoder("..--- ----- .---- ---..") == "2018"
+    assert morse_decoder(".. -   .-- .- ...   .-   --. --- --- -..   -.. .- -.--") == "It was a good day"
+    print("Coding complete? Click 'Check' to earn cool rewards!")
