7897cff632dd458da17ffa16c75ece1c133acdd7
vit-aborigen <vit.aborigen@gmail.com>
2018-11-15 19:54:12 +0300

    Party bit generator

---
+++ Parity Bit Generator.py
@@ -0,0 +1,16 @@
+checkio = lambda x: ''.join([chr(int(bin(value>>1)[2:], 2)) for value in x if not bin(value).count('1') % 2])
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio([135, 134, 124, 233,
+                    209, 81, 42, 202,
+                    198, 194, 229, 215,
+                    230, 146, 28, 210,
+                    145, 137, 222, 158,
+                    49, 81, 214, 157]) == "Checkio"
+    assert checkio([144, 100, 200, 202,
+                    216, 152, 164, 88,
+                    216, 222, 65, 218,
+                    175, 217, 248, 222,
+                    171, 228, 216, 205,
+                    254, 201, 193, 220]) == "Hello World"
