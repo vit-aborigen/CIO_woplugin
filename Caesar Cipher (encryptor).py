4ce963f8dfbe96b05c4bb5380e102301e8cc9f0f
vit-aborigen <vit.aborigen@gmail.com>
2018-11-17 01:51:43 +0300

    CC done

---
+++ Caesar Cipher (encryptor).py
@@ -0,0 +1,22 @@
+from string import ascii_lowercase as alpha
+
+def to_encrypt(text, delta):
+    result = ''
+    for value in text:
+        if value in alpha:
+            result += alpha[(alpha.index(value) + delta) % 26]
+        else:
+            result += value
+    return result
+
+if __name__ == '__main__':
+    print("Example:")
+    print(to_encrypt('abc', 10))
+
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert to_encrypt("a b c", 3) == "d e f"
+    assert to_encrypt("a b c", -3) == "x y z"
+    assert to_encrypt("simple text", 16) == "iycfbu junj"
+    assert to_encrypt("important text", 10) == "swzybdkxd dohd"
+    assert to_encrypt("state secret", -13) == "fgngr frperg"
+    print("Coding complete? Click 'Check' to earn cool rewards!")
