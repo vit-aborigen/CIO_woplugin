d5355967e9714785e412eb8d50eea96e95c54cae
vit-aborigen <vit.aborigen@gmail.com>
2018-11-21 21:37:25 +0300

    Correct sentence

---
+++ Correct Sentence.py
@@ -0,0 +1,16 @@
+def correct_sentence(text: str) -> str:
+    return text[0].upper() + (text[1:] if text.endswith('.') else text[1:] + '.')
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(correct_sentence("greetings, friends"))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert correct_sentence("greetings, friends") == "Greetings, friends."
+    assert correct_sentence("Greetings, friends") == "Greetings, friends."
+    assert correct_sentence("Greetings, friends.") == "Greetings, friends."
+    assert correct_sentence("hi") == "Hi."
+    assert correct_sentence("welcome to New York") == "Welcome to New York."
+
+    print("Coding complete? Click 'Check' to earn cool rewards!")
