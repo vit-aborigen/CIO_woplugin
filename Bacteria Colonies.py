bee5ca7ef3f8af2561b448319164d62607f92897
vit-aborigen <vit.aborigen@gmail.com>
2018-12-03 15:33:35 +0300

    Bacteria colonies half

--- Bacteria Colonies.py
+++ Bacteria Colonies.py
@@ -1,7 +1,24 @@
+'''
+Algorithm:
+1) Disjoint set (start_new_colony) to distinct all separate colonies
+2) Generate perfect colonies (will be used as the mask / pattern) with the [2-5] side-length with the center at (0,0) point
+3) Calculate size of all perfect colonies (magic numbers)
+4) Compare size of each found colony with the perfect colonies. Leave colonies that fit to magic numbers
+5) Adjust
+
+
+'''
+
 from collections import deque
 
-def generate_perfect_colonies(max_len=9):
-    pass
+def generate_perfect_colonies(max_len=7):
+    perfect_colonies = {}
+    for size in range(1, max_len):
+        perfect_colony = []
+        for i in range(-size, size + 1):
+            perfect_colony += [(i, j) for j in range(-size + abs(i), size - abs(i) + 1)]
+        perfect_colonies[len(perfect_colony)] = perfect_colony
+    return perfect_colonies
 
 def start_new_colony(grid, start):
     neighbours = [(1, 0), (-1, 0), (0, 1), (0, -1)]
@@ -31,16 +48,17 @@ def healthy(grid):
         for j in range(w):
             if grid[i][j] and not is_bacteria_belong_to_any_colony((i,j), colonies):
                 colonies.append(start_new_colony(grid, (i, j)))
-
+    colonies = [colony for colony in colonies if len(colony) in perfect_colonies.keys()]
     return colonies
 
+print(healthy(((0,0,0,0,0,1,0,0,0,0,1,1,0,0,0),(0,0,0,0,1,1,1,0,0,0,1,1,1,0,0),(0,0,0,0,0,1,0,0,0,1,1,1,1,1,0),(0,0,0,0,0,0,0,0,1,1,1,1,1,1,1),(0,0,0,1,0,0,0,0,0,1,1,1,1,1,0),(0,0,1,1,1,0,0,0,0,0,1,1,1,0,0),(0,1,1,1,1,1,0,0,0,0,0,1,0,0,0),(1,1,1,1,1,1,1,0,0,0,0,0,0,0,0),(0,1,1,0,1,1,0,0,0,0,1,0,0,0,0),(0,0,1,1,1,1,0,0,0,1,1,1,0,0,0),(0,0,0,1,0,0,0,0,1,1,1,1,1,0,0),(0,0,0,0,0,0,0,1,1,1,1,1,1,1,0),(0,0,0,0,0,0,0,0,1,1,1,1,1,0,0),(0,0,0,0,0,0,0,0,0,1,1,1,0,0,0),(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0))))
 
 # def healthy(grid):
 #     return 0, 0
 
-print(healthy(((0, 1, 0),
-         (1, 1, 1),
-         (0, 1, 0),)))
+# print(healthy(((0, 1, 0),
+#          (1, 1, 1),
+#          (0, 1, 0),)))
 
 # if __name__ == '__main__':
 #     # These "asserts" using only for self-checking and not necessary for auto-testing
