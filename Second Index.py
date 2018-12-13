ea4f69d1d350c3fe1e84b6ae719193b36bac0e59
vit-aborigen <vit.aborigen@gmail.com>
2018-12-13 16:07:45 +0300

    Second index done

---
+++ Second Index.py
@@ -0,0 +1,21 @@
+def second_index(text: str, symbol: str) -> [int, None]:
+    flag = 0
+    for i, char in enumerate(text):
+        if char == symbol:
+            flag += 1
+            if flag == 2:
+                return i
+    return None
+
+
+if __name__ == '__main__':
+    print('Example:')
+    print(second_index("sims", "s"))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert second_index("sims", "s") == 3, "First"
+    assert second_index("find the river", "e") == 12, "Second"
+    assert second_index("hi", " ") is None, "Third"
+    assert second_index("hi mayor", " ") is None, "Fourth"
+    assert second_index("hi mr Mayor", " ") == 5, "Fifth"
+    print('You are awesome! All tests are done! Go Check it!')
