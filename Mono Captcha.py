f6f5e7df71c221eeaca91e7be5fe07a96cb1c9ac
vit-aborigen <vit.aborigen@gmail.com>
2018-12-19 15:37:15 +0300

    Mono captcha done

--- Mono Captcha.py
+++ Mono Captcha.py
@@ -15,19 +15,38 @@ def convert_font_to_single_digits_dict(font):
             digits[digit].append(line[i:i+3].replace('-', '0').replace('X', '1'))
     return digits
 
-def split_image_on_single_digits(image):
+def split_image_on_chunks(image):
     separator = (0, 0, 0, 0, 0)
-    digit, digits = [], []
+    part, parts = [], []
     for row in zip(*image):
         if row != separator:
-            digit.append(row)
+            part.append(row)
         else:
-            if digit:
+            if part:
                 tmp = []
-                for row in zip(*digit):
+                for row in zip(*part):
                     tmp.append(''.join([str(d) for d in row]))
-                digits.append(tmp)
+                parts.append(tmp)
+                part = []
+    return parts
+
+def restore_digit_from_parts(parts):
+    digit, digits = [], []
+    separator = ('0', '0', '0', '0', '0')
+    for part in parts:
+        if len(part[0]) == 1:
+            if digit:
+                digits.append(merge_parts(digit, separator, part))
                 digit = []
+            else:
+                digit = part
+        elif digit:
+            digits.append(merge_parts(separator, digit, separator))
+            digit = []
+        if len(part[0]) == 3:
+            digits.append(part)
+        elif len(part[0]) == 2:
+            digits.append(merge_parts(part, separator))
     return digits
 
 def compare_digit_from_image_with_the_standard(digit, standard, tolerance = 1):
@@ -39,12 +58,20 @@ def compare_digit_from_image_with_the_standard(digit, standard, tolerance = 1):
             return str(mask)
     return -1
 
+def merge_parts(*args):
+    digit, line = [], []
+    for idx in range(len(args[0])):
+        for part in args:
+            line.append(part[idx])
+        digit.append(''.join(line))
+        line = []
+    return digit
 
 def checkio(image: List[List[int]]) -> int:
     ocred = ''
     font = convert_font_to_single_digits_dict(FONT)
-    for digit in split_image_on_single_digits(image):
-        print(digit)
+    parts = split_image_on_chunks(image)
+    for digit in restore_digit_from_parts(parts):
         result = compare_digit_from_image_with_the_standard(digit, font)
         if result == -1:
             return 'Not found'
