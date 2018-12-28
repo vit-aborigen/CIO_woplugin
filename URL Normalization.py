24a5767d37125a849cad27088cbda3b8cfe68fdf
vit-aborigen <vit.aborigen@gmail.com>
2018-12-29 00:04:20 +0300

    URL normalization done

--- URL Normalization.py
+++ URL Normalization.py
@@ -1,4 +1,5 @@
 from itertools import chain
+import string
 
 def checkio(url):
     percent_positions = list(chain(*[(idx + 1, idx + 2) for idx, char in enumerate(url) if char == '%' if idx <= len(url) - 3]))
@@ -8,20 +9,35 @@ def checkio(url):
     first_rule = convert_to_lowercase(url)
     second_rule = capitalize_letters_in_escape_seq(first_rule)
     third_rule = decode_octets(second_rule, percent_positions)
-    fourth_rule = ''.join(third_rule).replace(':80/', '')
-    print(fourth_rule)
-    return fourth_rule
-
+    fourth_rule = remove_port_80(third_rule)
+    fifth_rule = remove_dot_segments(fourth_rule)
+    return fifth_rule
 
 def decode_octets(url, positions):
     for value in positions[::2]:
         char_ascii = url[value] + url[value+1]
         char = chr(int(char_ascii, 16))
-        if char in ['-', '.', '_', '~'] or char.isalnum():
-            url[value-1:value+2] = ('2del', char, '2del')
+        if char in ['-', '.', '_', '~'] or char in string.ascii_letters or char.isdigit():
+            url[value-1:value+2] = ('2del', char.lower(), '2del')
     return filter(lambda a: a != '2del', url)
 
+def remove_port_80(url):
+    without_80 = ''.join(url).replace(':80/', '/')
+    return without_80[:-3] if without_80.endswith(':80') else without_80
 
+def remove_dot_segments(url):
+    folders = []
+    for directory in url.split('/'):
+        if directory == '..':
+            folders = folders[:-1]
+        elif directory == '.':
+            continue
+        else:
+            folders.append(directory)
+    return '/'.join(folders)
+
+
+print(checkio("http://www.checkio.org/task/./1/../2/././name"))
 #These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':
     # assert checkio("Http://Www.Checkio.org") == \
