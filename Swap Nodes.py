25536c7b9190b7bbcf2c794a93cf161cef546990
vit-aborigen <vit.aborigen@gmail.com>
2019-01-28 14:16:49 +0300

    Swap nodes

---
+++ Swap Nodes.py
@@ -0,0 +1,18 @@
+def swap_nodes(a):
+    result = []
+    for i in range(0, len(a) - 1, 2):
+        result += [a[i+1], a[i]]
+    return result if not len(a) % 2 else result + [a[-1]]
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(list(swap_nodes([1, 2, 3, 4])))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert list(swap_nodes([1, 2, 3, 4])) == [2, 1, 4, 3]
+    assert list(swap_nodes([5, 5, 5, 5])) == [5, 5, 5, 5]
+    assert list(swap_nodes([1, 2, 3])) == [2, 1, 3]
+    assert list(swap_nodes([3])) == [3]
+    assert list(swap_nodes(["hello", "world"])) == ["world", "hello"]
+    print("Coding complete? Click 'Check' to earn cool rewards!")
